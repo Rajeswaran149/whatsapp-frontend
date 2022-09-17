01c07af87c1b8d1eaeb068c303d9bf6a59ef4439
Rajeswaran149 <rajeswaran149@gmail.com>
2022-09-17 22:03:49 +0530

    whatsapp-mern

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import React from "react"
+import React, {  useEffect } from "react"
 import './App.css';
 import Chat from "./Chat";
 import Sidebar from "./Sidebar";
+import Pusher from "pusher";
 
 function App() {
+
+useEffect(() => {
+  const pusher = new Pusher('fd3582e39042f7218e28', {
+    cluster: 'ap2'
+  });
+
+  const channel = pusher.subscribe('messages');
+    channel.bind('inserted', (data) => {
+      alert(JSON.stringify(data));
+    });
+
+},[])
+
   return (
     <div className="app">
        <div className="app__body">
